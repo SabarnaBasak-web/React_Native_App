ba5950830fe83c563bb0d4574e5b2a4b2d2e4e94
Sankha <sabarnabasak@gmail.com>
2021-10-08 13:13:55 +0530

    React Native App

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>My First React Native App </Text>
+    </View>
+  );
+};
+
+export default App;
